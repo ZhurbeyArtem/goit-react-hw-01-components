d3935ccb86061864ef495e224f612f484d2b28fc
Zhurbey Artem <zhurbeyartem@gmail.com>
2024-01-03 19:47:28 +0200

    fix

--- src/components/friends/friends.item.jsx
+++ src/components/friends/friends.item.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from 'react';
 import s from './style.module.css';
 
-const FriendsItem = ({ props }) => {
-  const {id, isOnline, name, avatar} = props
+const FriendsItem = ({ isOnline, name, avatar }) => {
   return (
-    <li key={id} className={s.friend}>
-      <span
-        className={`${s.status} ${isOnline ? s.online : s.offline}`}
-      ></span>
+    <li className={s.friend}>
+      <span className={`${s.status} ${isOnline ? s.online : s.offline}`}></span>
       <img className={s.friendAvatar} src={avatar} alt="avatar" />
       <span className={s.name}>{name}</span>
     </li>
   );
-}
+};
 
-export default FriendsItem
+export default FriendsItem;
--- src/components/friends/friends.jsx
+++ src/components/friends/friends.jsx
@@ -5,8 +5,13 @@ import FriendsItem from "./friends.item";
 const Friends = ({ props }) => {
   return (
     <ul className={s.friendList}>
-      {props.map((el) => (
-       <FriendsItem props={{id: el.id, isOnline: el.isOnline, name: el.name, avatar: el.avatar}} />
+      {props.map(el => (
+        <FriendsItem
+          key={el.id}
+          isOnline={el.isOnline}
+          name={el.name}
+          avatar={el.avatar}
+        />
       ))}
     </ul>
   );
